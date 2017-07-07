0a93e7884e48775a17c2020a6b0aaaf032504511
Ayoze Vera <ayo.verad@gmail.com>
2017-07-07 13:14:34 +0100

    refactor: tutorial commponents moved to components folder, index js needed

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+export default class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Board } from './Board';
+export { default as Square } from './Square';
+export { default as Game } from './Game';
