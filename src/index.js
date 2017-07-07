d592be6a87ac051ef179c78c83ba0ec77650a640
Ayoze Vera <ayo.verad@gmail.com>
2017-07-07 13:16:45 +0100

    import other way

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Board, Game, Square } from './components/';
+/* Using this we won't need the index.js file on components folder
+  import Board from './components/Board';
+  import Square from './components/Square';
+  import Game from './components/Game';
+*/
 import './index.css';
 
 // ========================================
