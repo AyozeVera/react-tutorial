dbca3162ab117581c8485576a9bfd2d3f499616c
Ayoze Vera <ayo.verad@gmail.com>
2017-07-07 13:40:36 +0100

    an interactive component, working with states

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,17 @@ import React from 'react';
 import '../index.css';
 
 export default class Square extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      value: null
+    }
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
