865291a8d955a6a103d9cac97c407704b99f96ef
Ayoze Vera <ayo.verad@gmail.com>
2017-07-07 14:28:15 +0100

    concept comment on Square

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../index.css';
 
 export default class Square extends React.Component {
+  // Cause this component dont controll its own value,
+  // it's called CONTROLLED COMPONENT
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
